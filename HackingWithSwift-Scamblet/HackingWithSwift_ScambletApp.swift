66411e392761ef4c4a19cf397285bb0235031ed2
Michael Jones <michael.jones90@me.com>
2026-03-29 09:30:25 +0100

    Initial Commit

---
+++ HackingWithSwift-Scamblet/HackingWithSwift_ScambletApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_ScambletApp.swift
+//  HackingWithSwift-Scamblet
+//
+//  Created by Michael Jones on 29/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_ScambletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
